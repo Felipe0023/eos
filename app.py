73d3a5d960b5c62b841ecf1385f6a487f19e503a
Felipe0023 <149027192+Felipe0023@users.noreply.github.com>
2026-06-01 12:39:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -171,11 +171,11 @@ with st.sidebar:
     /* Zonas de Carga (Azul Acero Claro con contraste alto) */
     [data-testid="stSidebar"] [data-testid="stForm"] [data-testid="stFileUploaderDropzone"] {
         background-color: #1E293B !important; 
-        border: 2px solid #38BDF8 !important; border-radius: 8px !important; #solid
+        border: 2px dashed #38BDF8 !important; border-radius: 8px !important; #solid
     }
     /* Texto Informativo de Archivos (Blanco Nítido) */
     [data-testid="stSidebar"] [data-testid="stForm"] [data-testid="stFileUploaderDropzone"] p {
-        color: #FFFFFF !important; font-size: 13px !important;
+        color: #F1F5F9 !important; font-size: 13px !important;
     }
 
     /* Botón interno Upload */
