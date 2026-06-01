820f1f3b151af4ae8840d7d77ddb42b86626aaf5
Felipe0023 <149027192+Felipe0023@users.noreply.github.com>
2026-06-01 12:32:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ with st.sidebar:
         border: 1px solid #334155; border-radius: 12px; padding: 20px;
     }
     [data-testid="stSidebar"] [data-testid="stForm"] h3 { color: #FFFFFF !important; }
-    [data-testid="stSidebar"] [data-testid="stForm"] label { color: #E2E8F0 !important; }
+    [data-testid="stSidebar"] [data-testid="stForm"] label { color: #FFFFFF !important; } ##E2E8F0 
 
     /* Zonas de Carga (Azul Acero Claro con contraste alto) */
     [data-testid="stSidebar"] [data-testid="stForm"] [data-testid="stFileUploaderDropzone"] {
